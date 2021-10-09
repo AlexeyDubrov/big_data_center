edabe579717c16f2c460e8135dbe282005bf6f31
AlexeyDubrov <dubrov.aleksei@gmail.com>
2021-10-09 20:01:01 +0300

    authapp is ready

--- bdc_test/bdc_test/settings.py
+++ bdc_test/bdc_test/settings.py
@@ -122,4 +122,8 @@ AUTH_USER_MODEL = 'authapp.User'
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'project.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'apps.authentication.backends.JWTAuthentication',
+    )
 }
